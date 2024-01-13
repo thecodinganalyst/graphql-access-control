97e96589dd74182076556b88c88ed619192c5b7e
Dennis Cai <dennis@thecodinganalyst.com>
2024-01-13 16:47:07 +0800

    add custom field visibility

--- src/main/java/com/hevlar/graphql/configuration/GraphqlConfiguration.java
+++ src/main/java/com/hevlar/graphql/configuration/GraphqlConfiguration.java
@@ -1,8 +1,14 @@
 package com.hevlar.graphql.configuration;
 
+import com.hevlar.graphql.graphql.CustomFieldVisibility;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 public class GraphqlConfiguration {
-
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer(){
+        return builder -> builder.fieldVisibility(new CustomFieldVisibility());
+    }
 }
--- src/main/java/com/hevlar/graphql/graphql/CustomFieldVisibility.java
+++ src/main/java/com/hevlar/graphql/graphql/CustomFieldVisibility.java
@@ -3,17 +3,14 @@ package com.hevlar.graphql.graphql;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLFieldsContainer;
 import graphql.schema.visibility.GraphqlFieldVisibility;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 @RequiredArgsConstructor
 public class CustomFieldVisibility implements GraphqlFieldVisibility {
-
-    @NonNull
-    final SecurityContext securityContext;
 
     @Override
     public List<GraphQLFieldDefinition> getFieldDefinitions(GraphQLFieldsContainer fieldsContainer) {
@@ -22,6 +19,6 @@ public class CustomFieldVisibility implements GraphqlFieldVisibility {
 
     @Override
     public GraphQLFieldDefinition getFieldDefinition(GraphQLFieldsContainer fieldsContainer, String fieldName) {
-        return null;
+        return fieldsContainer.getFieldDefinition(fieldName);
     }
 }
